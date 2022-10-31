7535b7c8a8aeb298f921ab5c46dc823f8dbf1f9f
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-31 11:42:29 +0600

    router changes location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import { router } from './Routes/Route/Router';
+import { router } from './Router/Routes/Routes';
+
 
 function App() {
   return (
