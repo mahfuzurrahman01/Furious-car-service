4b649a7651674c1a117492be560d62852517973c
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-31 12:06:11 +0600

    footer component setup done

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
+import Footer from '../../Shared/Header/Navbar/Footer/Footer';
+import Navbar from '../../Shared/Header/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
